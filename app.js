898e8fddc68897acfdfe5eb43d19056914ff12c7
JKP-2001 <jaypandey6957@gmail.com>
2022-03-09 12:32:00 +0530

    Add gitignore

--- app.js
+++ app.js
@@ -205,6 +205,10 @@ app.get('/auth/microsoft/secrets',
         res.redirect('/secrets');
     });
 
-app.listen(3000, function () {
-    console.log("3000 port running");
-});
+    let port = process.env.PORT;
+    if (port == null || port == "") {
+      port = 3000;
+    }
+    app.listen(port,function(){
+        console.log("Server Has Started");
+    });
